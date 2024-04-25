2b09cc78b3a8e1716d5ed16203e73c4b70469f89
Mohammad Ashif <Mohammad Ashif>
2024-04-25 10:40:09 +0600

    typescript function

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+## Understanding TypeScript Functions
+
+- Variables can be assigned functions for later invocation.
+- Function parameters can be typed, such as `number` or `string`, in TypeScript.
+- Default parameter values can be specified or set for function parameters.
+- TypeScript can infer the return type of functions, or you can explicitly define it.
+- Functions that don't return a value are implicitly typed as `void`.
---
+++ src/sandbox.ts
@@ -0,0 +1,38 @@
+// let greet = () => {
+//     console.log("hello");
+// }
+
+// greet = 'hello' //! We can't do this because typescript automatically identify
+
+let greet: Function; //! We can explicitly say that this will be a function
+
+greet = () => {
+    console.log("hello, again");
+};
+
+const add = (a: number, b: number, c?: number | string) => {
+    //! We can pass an optional parameter like this if we don't any value it will be undefined
+    console.log(a + b);
+    console.log(c);
+};
+
+add(5, 8);
+
+// ! When a Typescript function doesn't return anything, it returns void
+
+const sum = (a: number, b: number, c: number | string = 5) => {
+    //! We can also pass a default value. When we pass default value, we will not use question mark
+    console.log(a + b);
+    console.log(c);
+};
+
+sum(4, 9, "99");
+
+// ! We will always pass the required parameters at the first and place the optional parameters last
+
+const minus = (a: number, b: number) => { 
+    return a - b;
+};
+
+const result = minus(10, 5); //! When a function returns something, TypeScript infers the return type automatically. We can also define the return type explicitly.
+console.log(result);
